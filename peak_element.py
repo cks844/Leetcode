4cc61dd7f70160442adae226e816e574217c0ac4
cks844 <cyrilksony@gmail.com>
2024-07-17 12:41:31 +0530

    update

---
+++ peak_element.py
@@ -0,0 +1,17 @@
+"""A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, 
+return the index to any of the peaks.
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6."""
+
+def findPeakElement(self, nums):
+        return nums.index(max(nums))
