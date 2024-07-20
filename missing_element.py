b4a7f0c3153f05f9cdaf5aa45eb04767d0d6f0d5
cks844 <cyrilksony@gmail.com>
2024-07-20 16:08:57 +0530

    update

--- missing_element.py
+++ missing_element.py
@@ -1,3 +1,11 @@
+"Solution-1"
+def missing_element(arr:list) -> int:
+    for num in range(arr[0],len(arr)):
+        if num not in arr:
+            return print(num)
+missing_element([1,3,4])
+
+"Solution-2"
 def missing_element(arr:list) -> int:
     cur_sum = 0
     for i in arr:
@@ -6,4 +14,4 @@ def missing_element(arr:list) -> int:
     for i in range(arr[0],arr[len(arr)-1]+1):
         req_sum += i
     return print(req_sum - cur_sum)
-missing_element([1,2,3,5])
+missing_element([1,3,4])
