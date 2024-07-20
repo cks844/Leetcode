ac618947754f8bceacefe3a1f9632a3219d88df6
cks844 <cyrilksony@gmail.com>
2024-07-20 16:39:18 +0530

    new

---
+++ sum_squares.py
@@ -0,0 +1,19 @@
+def sum_squares(num:int):
+    sq_list = [i*i for i in range(1,num)]
+    new_list =[]
+    for i in range(0,len(sq_list)):
+        for j in range(1,len(sq_list)):
+            if sq_list[i] + sq_list[j] == num:
+                new_list.append(sq_list[i])
+                new_list.append(sq_list[j])
+    
+    if not new_list:
+        return print("No squares with a sum",num)
+    else:
+        return print(set(new_list))
+        
+if __name__ == "__main__":
+    num = int(input("Enter the number:"))
+    sum_squares(num)
+        
+       
