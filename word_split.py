499bdcb73cfd1f1d439d7729c6019b5d0dd2dff7
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2026-01-15 01:20:17 +0530

    new solution
    Refactor word_split function to improve clarity and functionality.

--- word_split.py
+++ word_split.py
@@ -1,17 +1,33 @@
 "Split sentences without using split function"
 
-def word_split(sen):
-    word = ""
-    words=[]
-    for char in sen:
+def word_split(sent):
+    words = ""
+    split_words = []
+    for char in sent:
         if char != " ":
-            word += char
-        else:
-            if word:
-                words.append(word)
-                word =""
-    if word:
-        words.append(word)
-    return words
+            words += char
+        elif words:
+            split_words.append(words)
+            words =""
+    if words:
+        split_words.append(words)
+
+    return split_words
 inp = input("Enter the sentence:")
 word_split(inp)    
+
+def word_split(sent):
+    words = ""
+    split_words = []
+    for char in sent:
+        if char != " ":
+            words += char
+        elif len(words) != 0:
+            split_words.append(words)
+            words = ""
+    if len(words) != 0:
+            split_words.append(words)
+
+    return split_words
+inp = input("Enter the sentence:")
+print(word_split(inp))
